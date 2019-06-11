d698f0c4184a59e3df8fa11d9f354894bbe774ef
Loganbucket <slogan@ssmic.com>
2019-06-11 14:39:35 -0400

    Add Course entity

--- CleanArch/CleanArch.Domain/Entities/Course.cs
+++ CleanArch/CleanArch.Domain/Entities/Course.cs
@@ -4,7 +4,11 @@ using System.Text;
 
 namespace CleanArch.Domain.Entities
 {
-    class Course
+    public class Course
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
